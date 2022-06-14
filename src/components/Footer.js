88f627da7a0b476813a1f048497add9117c7da18
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-06-14 22:33:45 +0700

    add staff list view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,56 +11,71 @@ function Footer() {
 						<h5>Link</h5>
 						<ul className="list-unstyled">
 							<li>
-								<Link to="/home">Home</Link>
+								<Link to="/staff">Staff</Link>
 							</li>
 							<li>
-								<Link to="/aboutus">About</Link>
+								<Link to="/dept">Department</Link>
 							</li>
 							<li>
-								<Link to="/menu">Menu</Link>
-							</li>
-							<li>
-								<Link to="/contactus">contact</Link>
+								<Link to="/pay">PaySlip</Link>
 							</li>
 						</ul>
 					</div>
 					<div className="col-7 col-sm-5">
-						<h5>Our Address</h5>
-						<address>Lorem ipsum dolor sit amet consectetur adipisicing elit.</address>
-						<i className="fa fa-phone fa-lg">0379496709</i>
-						<i className="fa fa-fax fa-lg">+845648692</i>
+						<h3>About Us</h3>
+						<h4>EMEC CO. LTD</h4>
+						<address>Địa chỉ: VJ9R+6PF, Đường An Trì, Hùng Vương, Hồng Bàng, Hải Phòng</address>
+						<i className="fa fa-phone fa-lg">
+							<span style={{ marginLeft: "20px" }}>0379496709</span>
+						</i>
+						<br />
+						<i className="fa fa-fax fa-lg">
+							<span style={{ marginLeft: "20px" }}>+845648692</span>
+						</i>
+						<br />
+
 						<i className="fa fa-envelope fa-lg">
-							: <a href="">LTB.199X@OUTLOOK.COM</a>
+							<a href="">
+								<span style={{ marginLeft: "20px" }}>ltb.199x@outlook.com</span>
+							</a>
 						</i>
 					</div>
 					<div className="col-12 col-sm-4 align-self-center">
 						<div className="text-center">
-							<a className="btn btn-social-icon btn-google" href="http://google.com/+">
+							<a
+								style={{ margin: "4px" }}
+								className="btn btn-social-icon btn-google"
+								href="http://google.com/+">
 								<i className="fa fa-google-plus"></i>
 							</a>
 							<a
+								style={{ margin: "4px" }}
 								className="btn btn-social-icon btn-facebook"
 								href="http://www.facebook.com/profile.php?id=">
 								<i className="fa fa-facebook"></i>
 							</a>
-							<a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/">
+							<a
+								style={{ margin: "4px" }}
+								className="btn btn-social-icon btn-linkedin"
+								href="http://www.linkedin.com/in/">
 								<i className="fa fa-linkedin"></i>
 							</a>
-							<a className="btn btn-social-icon btn-twitter" href="http://twitter.com/">
+							<a
+								style={{ margin: "4px" }}
+								className="btn btn-social-icon btn-twitter"
+								href="http://twitter.com/">
 								<i className="fa fa-twitter"></i>
 							</a>
-							<a className="btn btn-social-icon btn-google" href="http://youtube.com/">
+							<a
+								style={{ margin: "4px" }}
+								className="btn btn-social-icon btn-google"
+								href="http://youtube.com/">
 								<i className="fa fa-youtube"></i>
 							</a>
-							<a className="btn btn-social-icon" href="mailto:">
+							<a style={{ margin: "4px" }} className="btn btn-social-icon" href="mailto:">
 								<i className="fa fa-envelope-o"></i>
 							</a>
 						</div>
-					</div>
-				</div>
-				<div className="row justify-content-center">
-					<div className="col-auto">
-						<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima assumenda</p>
 					</div>
 				</div>
 			</div>
--- src/components/Staff.js
+++ src/components/Staff.js
@@ -82,59 +82,25 @@ class Staff extends Component {
 		});
 
 		return (
-			<div className="container mt-3">
-				<div className="row justify-content-between">{StaffList}</div>
-				<div className="container mb-2">
-					<div className="row  justify-content-between">
-						<button
-							data-index="1"
-							disabled={this.state.disableBtnId === 1}
-							onClick={(e) => {
-								this.handleResetLayout(e.target.dataset.index);
-							}}
-							className="btn btn-danger mt-2 mr-2 col-12 col-sm-4 col-md-2">
-							Reset Layout
-						</button>
-
-						<button
-							data-index="2"
-							disabled={this.state.disableBtnId === 2}
-							onClick={(e) => {
-								this.handleChangeToOneColLayout(e.target.dataset.index);
-							}}
-							className="btn btn-primary mt-2 mr-2  col-12 col-sm-4 col-md-2">
-							1 Column
-						</button>
-						<button
-							data-index="3"
-							disabled={this.state.disableBtnId === 3}
-							onClick={(e) => {
-								this.handleChangeToTwoColLayout(e.target.dataset.index);
-							}}
-							className="btn btn-secondary mt-2  mr-2 col-12 col-sm-4 col-md-2">
-							2 Columns
-						</button>
-						<button
-							data-index="4"
-							disabled={this.state.disableBtnId === 4}
-							onClick={(e) => {
-								this.handleChangeToThreeColLayout(e.target.dataset.index);
-							}}
-							className="btn btn-success mt-2 mr-2 col-12 col-sm-4 col-md-2">
-							3 Columns
+			<div className="container mt-3 ">
+				<div className="row justify-content-between">
+					<div className="col-12 col-lg-6">
+						<h3>Staff List</h3>
+					</div>
+					<div className="col-12 col-lg-6 text-center row">
+						<input
+							type="search"
+							className=" col-9 form-control rounded "
+							placeholder=" Type Staff Name"
+							aria-label="Search"
+						/>
+						<button type="button" class="btn btn-primary ml-1">
+							Search
 						</button>
 					</div>
 				</div>
-
-				<p className="mt-1" style={this.state.style}>
-					Bấm vào tên nhân viên để xem thông tin chi tiết
-				</p>
-
-				<div className="row mt-3">
-					<div className="col-12 col-md-5 ">
-						<StaffDetail selectedStaff={this.state.selectedStaff} />
-					</div>
-				</div>
+				<hr />
+				<div className="row justify-content-between mb-3">{StaffList}</div>
 			</div>
 		);
 	}
