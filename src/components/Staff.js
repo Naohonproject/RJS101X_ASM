a96f6b31c0316e5fa9cae1d3f69d8f4321673260
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-06-17 01:02:39 +0700

    add search feature

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -20,8 +20,54 @@ class Staff extends Component {
 				</div>
 			);
 		});
+		function getName(list, name) {
+			let reg = new RegExp(name);
+			let result = [];
+			list.forEach(function (item) {
+				if (item.name.search(reg) > -1) {
+					result.push(item);
+				}
+			});
+			return result;
+		}
 
-		return <div className="row mb-4 mt-3">{StaffList}</div>;
+		const handleSearch = (value) => {
+			const newStaffs = getName(STAFFS, value);
+			this.setState({
+				...this.state,
+				staffs: newStaffs,
+			});
+		};
+
+		return (
+			<React.Fragment>
+				<div className="row align-items-center mt-4 justify-content-around">
+					<h3 className="col-12 col-md-4">Staffs</h3>
+					<div className="col-12 col-md-4 text-center row align-item-center">
+						<input
+							style={{
+								display: "inline-block",
+								height: "38px",
+								borderRadius: "4px",
+								border: "solid 1px #ccc",
+								padding: "8px",
+							}}
+							onChange={(e) => {
+								handleSearch(e.target.value);
+							}}
+							type="search"
+							placeholder="Search"
+							className="mx-2"
+						/>
+					</div>
+					<div className="col-12 col-md-4">
+						
+					</div>
+				</div>
+				<hr />
+				<div className="row mb-4 mt-3">{StaffList}</div>
+			</React.Fragment>
+		);
 	}
 }
 
