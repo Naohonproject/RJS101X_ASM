d31530fcd082715a6a32c1075f2c02218586f43a
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-07-09 01:37:10 +0700

    add fade in aimation for salary component

--- src/components/Salary.js
+++ src/components/Salary.js
@@ -1,12 +1,12 @@
 /** @format */
 
 import React from "react";
-import { connect } from "react-redux";
+
 import { Card, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from "react-router-dom";
 
 import { Loading } from "./Loading";
-import { staffsSubUrl } from "../shared/baseUrl";
+import { Fade, Stagger } from "react-animation-components";
 
 function Salary({ staffs }) {
 	if (staffs.isLoading) {
@@ -15,15 +15,17 @@ function Salary({ staffs }) {
 		const salary = staffs.salary.map((staff) => {
 			return (
 				<div className="col-lg-4 col-md-6 col-12 my-2" key={staff.id}>
-					<Card>
-						<CardBody>
-							<CardTitle>{staff.name}</CardTitle>
-							<CardText>Mã nhân viên : {staff.id}</CardText>
-							<CardText>Hệ Số Lương : {staff.salaryScale}</CardText>
-							<CardText>Số ngày làm thêm : {staff.overTime}</CardText>
-							<CardText>Lương : {staff.salary}</CardText>
-						</CardBody>
-					</Card>
+					<Fade in>
+						<Card>
+							<CardBody>
+								<CardTitle>{staff.name}</CardTitle>
+								<CardText>Mã nhân viên : {staff.id}</CardText>
+								<CardText>Hệ Số Lương : {staff.salaryScale}</CardText>
+								<CardText>Số ngày làm thêm : {staff.overTime}</CardText>
+								<CardText>Lương : {staff.salary}</CardText>
+							</CardBody>
+						</Card>
+					</Fade>
 				</div>
 			);
 		});
@@ -40,7 +42,9 @@ function Salary({ staffs }) {
 						<h3 className="col-12">Salary</h3>
 						<hr />
 					</div>
-					<div className="row">{salary}</div>
+					<Stagger in>
+						<div className="row">{salary}</div>
+					</Stagger>
 				</div>
 			</React.Fragment>
 		);
